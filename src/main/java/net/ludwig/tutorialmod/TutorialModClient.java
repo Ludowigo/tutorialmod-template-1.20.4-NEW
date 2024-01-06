1f7d5f5152e418d278c90fa6d15085fefc02bab4
Ludwig <ludwig@w-waehnelt.de>
2024-01-06 15:31:31 +0100

    initial commit

---
+++ src/main/java/net/ludwig/tutorialmod/TutorialModClient.java
@@ -0,0 +1,10 @@
+package net.ludwig.tutorialmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TutorialModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
