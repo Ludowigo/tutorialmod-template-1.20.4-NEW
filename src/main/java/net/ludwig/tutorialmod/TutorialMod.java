94520b5be82c9c40d97d9ec30b6ee08b0a069656
Ludwig <ludwig@w-waehnelt.de>
2024-01-06 15:53:54 +0100

    Item added: Ruby (no texture, test only. Development paused for java learning series)

--- src/main/java/net/ludwig/tutorialmod/TutorialMod.java
+++ src/main/java/net/ludwig/tutorialmod/TutorialMod.java
@@ -2,6 +2,7 @@ package net.ludwig.tutorialmod;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.ludwig.tutorialmod.item.ModItems;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -11,6 +12,7 @@ public class TutorialMod implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
+		ModItems.registerModItems();
 
 		LOGGER.info("Hello Fabric world!");
 	}
